34a24cd64c0c5b39b8b1ce43cc1b71c31e3a520e
Oleg Martynov <olegman.ru@gmail.com>
2012-03-19 16:22:05 +0400

    Added new properties to Airplane

--- airplane.rb
+++ airplane.rb
@@ -6,10 +6,38 @@ class Airplane
     @length = 200
   end	
 
+  def aircraft_type
+    @aircraft_type    
+  end
+
+  def weight
+    @weight
+  end
+
+  def speed
+    @speed
+  end
+
+  def altitude
+    @altitude
+  end
+
+  def position_x
+    
+  end
+
+  def position_y
+    
+  end
+
+  def course
+    @course = 0..360
+  end
+
   def info
     "title: " + @title + " weight: " + @weight.to_s + " length: " + @length.to_s
   end
+
+  attr_writer :aircraft_type, :weight, :speed, :altitude, :position_x, :position_y, :course   
   
 end
-
-@item1 = Airplane.new
